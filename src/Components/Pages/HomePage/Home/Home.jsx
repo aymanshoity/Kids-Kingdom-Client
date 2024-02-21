db76ba71f1c103bd8a6cb9005810ced8fdd78705
Syeda Umme Ayman <aymanshoity@gmail.com>
2024-02-21 21:12:52 +0600

    Footer section is done

--- src/Components/Pages/HomePage/Home/Home.jsx
+++ src/Components/Pages/HomePage/Home/Home.jsx
@@ -1,10 +1,12 @@
 import Banner from "../Banner";
+import Footer from "../Footer";
 
 
 const Home = () => {
     return (
         <div className="">
             <Banner></Banner>
+            <Footer></Footer>
         </div>
     );
 };
